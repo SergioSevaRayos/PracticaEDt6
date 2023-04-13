c5bc595b5df9cf7911d39eb1b3d54fd8dbbe8605
SergioSevaRayos <sergiosevarayos@gmail.com>
2023-04-13 17:59:01 +0200

    La recolección de datos esta lista y funciona correctamente

--- src/practicaT6/EscribeFichero.java
+++ src/practicaT6/EscribeFichero.java
@@ -28,7 +28,7 @@ public class EscribeFichero {
 	static String edad; // Variable para almacenar la edad
 	static String calle; // Variable para almacenar la calle
 	static String localidad; // Variable para almacenar la localidad
-	static int cod_postal; // Variable para almacenar el código postal
+	static String cod_postal; // Variable para almacenar el código postal
 	static int op = 0; // Variable para la selección de las diferentes opciones
 	static Scanner sc = new Scanner(System.in); // Variable para escanear las diferentes opciones
 
@@ -37,45 +37,41 @@ public class EscribeFichero {
 	
 	public static void inDNI() {
 		// Recolección del dato "DNI"
+		sc.nextLine(); // Limpiamos el buffer de entrada
 		System.out.print("\nIntroduce el DNI del paciente: ");
-		DNI = sc.next();
+		DNI = sc.nextLine();
 		
 		if (DNI != null && DNI.matches("\\d{8}[A-Za-z]")) {
 			System.out.println("DNI correcto");
-			op = 1;
 			menu();
 		}else {
-			System.out.println("DNI incorrecto, vuelve a introducirlo");
+			System.out.print("DNI incorrecto, vuelve a introducirlo: ");
 			inDNI();
 		}
 	}
 	
 	public static void inNombre() {
 		// Recolección del dato "nombre"
-		nombre = "sergio";
-		while (nombre != null || !nombre.matches("^[a-zA-Z ]+$")) {
-			System.out.println("\nIntroduce el nombre del paciente: ");
+		op = 1;
+		sc.nextLine(); // Limpiamos el buffer de entrada
+		System.out.println("\nIntroduce el nombre del paciente: ");
+		nombre = sc.nextLine();
+		
+		if (nombre != "") {
+			menu();
+		}else {
 			nombre = sc.nextLine();
-			if (nombre.matches("^[a-zA-Z ]+$")) {
-			    System.out.println("Nombre correcto");
-			    op = 1;
-			    menu();
-			} else {
-			    System.err.println("Nombre incorrecto, vuelve a introducirlo");
-			    inNombre();
-			}
 		}
 	}
 	
 	public static void inEdad() {
 		// Recolección del dato "edad"
+		op = 1;
 		System.out.println("\nIntroduce la edad del paciente: ");
 		edad = sc.next();
 
-		
 		if (edad != null && edad.matches("^[0-9]+$")) {
 			System.out.println("Edad correcta");
-			op = 1;
 			menu();
 		}else {
 			System.err.println("Edad incorrecta, vuelve a introducirla");
@@ -86,48 +82,100 @@ public class EscribeFichero {
 
 	public static void inCalle() {
 		// Recolección del dato "calle"
+		op = 1;
 		System.out.println("\nIntroduce la calle del paciente: ");
 		calle = sc.nextLine();
+		if (calle != "") {
+			menu();
+		} else {
+			calle = sc.nextLine();
+		}
 		menu();
+		
 	}
 	
 	public static void inLocalidad() {
 		// Recolección del dato "localidad"
+		op = 1;
 		System.out.println("Introduce la localidad del paciente: ");
 		localidad = sc.nextLine();
+		if (localidad != "") {
+			menu();
+		} else {
+			localidad = sc.nextLine();
+		}
 		menu();
 	}
 	
 	public static void inCodPostal() {
 		// Recolección del dato "cod_postal"
+		op = 1;
 		System.out.println("\nIntroduce el cod_postal del paciente: ");
-		cod_postal = sc.nextInt();
+		cod_postal = sc.nextLine();
+		
+		if (cod_postal != "" ) {
+			menu();
+		} else {
+			cod_postal = sc.nextLine();
+		}
 		menu();
-	}
 
+	}
+	
+	public static void iniciaDatos() {
+		DNI = "";
+		nombre = "";
+		edad = "";
+		calle = "";
+		localidad = "";
+		cod_postal = "";
+		op = 0;
+	}
+	private static void muestraDatos() {
+		System.out.println("_______________________________________________________");
+		System.out.println("\t\t\tRESUMEN");
+		System.out.println("'''''''''''''''''''''''''''''''''''''''''''''''''''''''");
+		System.out.println("DNI: " + DNI);
+		System.out.println("Nombre: "+nombre);
+		System.out.println("Edad: "+ edad);
+		System.out.println("Calle: " + calle);
+		System.out.println("Localidad: "+ localidad);
+		System.out.println("Codigo postal: " + cod_postal);
+		System.out.println("_______________________________________________________");
+	}
 
 	
 	public static void menu() {
 		while (op <= 0 || op >2) {
-			System.out.println("\tMENU");
-			System.out.println("---------------------");
+			System.out.println("\t\t\t  MENU");
+			System.out.println("'''''''''''''''''''''''''''''''''''''''''''''''''''''''");
 			System.out.println("1. Agregar cliente");
 			System.out.println("2. Nueva visita");
 			System.out.println("");
 			System.out.println("0. Salir");
 			System.out.println("");
-			op = sc.nextInt();
+
+			if(sc.hasNextInt()) { // Si el siguiente valor del scanner es un int
+			    op = sc.nextInt(); // Lo leemos y lo asignamos a op
+			} else {
+			    sc.next(); // Limpiamos el buffer de entrada
+			    System.err.println("\nError: Opcion incorrecta");
+			    System.err.println("Error: Vuelve a elegir la opcion");
+			}
+
 			if (op < 0 || op > 2) {
-				System.err.println("\nError: Opcion incorrecta");
-				System.err.println("Error: Vuelve a elegir la opcion");
+			    System.err.println("\nError: Opcion incorrecta");
+			    System.err.println("Error: Vuelve a elegir la opcion");
 			}
 		}
 		switch (op) {
 		case 1:
 			op = 0;
 			while (op <= 0 || op >6 ) {
-				System.out.println("\tAgregar cliente");
-				System.out.println("---------------------");
+				muestraDatos();
+
+				System.out.println("\t\t    AGREGAR CLIENTE");
+				System.out.println("'''''''''''''''''''''''''''''''''''''''''''''''''''''''");
 				System.out.println("1. DNI");
 				System.out.println("2. Nombre");
 				System.out.println("3. Edad");
@@ -220,6 +268,8 @@ public class EscribeFichero {
 	//---------------------------------------------- Fin 
 	
 	public static void main(String[] args) {
+		iniciaDatos();
+		muestraDatos();
 		menu();
 	}
 	
